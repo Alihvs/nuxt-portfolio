4b939f28300d0811ee2427d51c4d9000741abee7
Ali Havasi <alihvs@Alis-MacBook-Air.local>
2018-08-24 20:50:49 +0430

    added bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,31 @@
 module.exports = {
+  head: {
+    titleTemplate: "%s - Nuxt.js",
+    title: "Nuxt Portfolio",
     head: {
-      titleTemplate: '%s - Nuxt.js',
-      title: 'Nuxt Portfolio',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'Meta description' }
+      script: [
+        { src: "https://code.jquery.com/jquery-3.3.1.slim.min.js" },
+        {
+          src:
+            "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        },
+        {
+          src:
+            "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        }
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href:
+            "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        }
       ]
-    }
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Meta description" }
+    ]
   }
+};
